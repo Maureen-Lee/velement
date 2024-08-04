ee1defaefb5ab0d11182aebca0866f0c2ddb142a
Nancy <547463699@qq.com>
2024-08-04 08:06:57 +0800

    docs: "记录组件库开发过程"

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,39 @@
+<template>
+    <button 
+    ref="b_ref"
+    class="vk-button"
+    :class="{
+    [`vk-button-${type}`]: type,
+    [`vk-button-size-${size}`]: size,
+    'is-disabled': disabled,
+    'is-round': round,
+    'is-circle': circle,
+    'is-plain' : plain,
+    }"
+    :disabled="disabled"
+    :type="nativeType"
+    >
+    <span> 
+        <slot></slot>
+    </span>
+    </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps } from './type'
+withDefaults(defineProps<ButtonProps>(),{
+    nativeType:'button',
+
+})
+
+const b_ref = ref<HTMLButtonElement>()
+
+defineExpose({
+    ref: b_ref
+})
+</script>
+
+<style scoped>
+
+</style>
